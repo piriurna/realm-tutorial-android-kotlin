eb5cae31f0f5ed3b977bb82e0802162669350fcd
Franco <Zalamena>
2021-07-02 16:10:00 +0100

    Added Google sign in Option.

--- app/src/main/java/com/mongodb/tasktracker/LoginActivity.kt
+++ app/src/main/java/com/mongodb/tasktracker/LoginActivity.kt
@@ -1,12 +1,23 @@
 package com.mongodb.tasktracker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.mongodb.tasktracker.model.login.GOOGLE_LOGIN_REQUEST
 import io.realm.mongodb.Credentials
+import io.realm.mongodb.auth.GoogleAuthType
+
+
 /*
 * LoginActivity: launched whenever a user isn't already logged in. Allows a user to enter email
 * and password credentials to log in to an existing account or create a new account.
@@ -16,6 +27,7 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var password: EditText
     private lateinit var loginButton: Button
     private lateinit var createUserButton: Button
+    private lateinit var googleSignIn : SignInButton
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,9 +36,16 @@ class LoginActivity : AppCompatActivity() {
         password = findViewById(R.id.input_password)
         loginButton = findViewById(R.id.button_login)
         createUserButton = findViewById(R.id.button_create)
+        googleSignIn = findViewById(R.id.google_sign_in_button)
+
+        googleSignIn.setSize(SignInButton.SIZE_WIDE);
 
         loginButton.setOnClickListener { login(false) }
         createUserButton.setOnClickListener { login(true) }
+        googleSignIn.setOnClickListener{ loginWithGoogle() };
+
+
+
     }
 
     override fun onBackPressed() {
@@ -51,6 +70,10 @@ class LoginActivity : AppCompatActivity() {
         else -> true
     }
 
+    //-----------
+    //LOGIN
+    //----------
+
     // handle user authentication (login) and account creation
     private fun login(createUser: Boolean) {
         if (!validateCredentials()) {
@@ -61,6 +84,7 @@ class LoginActivity : AppCompatActivity() {
         // while this operation completes, disable the buttons to login or create a new account
         createUserButton.isEnabled = false
         loginButton.isEnabled = false
+        googleSignIn.isEnabled = false
 
         val username = this.username.text.toString()
         val password = this.password.text.toString()
@@ -68,10 +92,82 @@ class LoginActivity : AppCompatActivity() {
 
         if (createUser) {
             // register a user using the Realm App we created in the TaskTracker class
-            // TODO: Register a new user with the supplied username and password when the "Create" button is pressed.
+            taskApp.emailPassword.registerUserAsync(username, password) {
+                // re-enable the buttons after user registration returns a result
+                createUserButton.isEnabled = true
+                loginButton.isEnabled = true
+                if (!it.isSuccess) {
+                    onLoginFailed("Could not register user.")
+                    Log.e(TAG(), "Error: ${it.error}")
+                } else {
+                    Log.i(TAG(), "Successfully registered user.")
+                    // when the account has been created successfully, log in to the account
+                    login(false)
+                }
+            }
         } else {
-            // TODO: Log in with the supplied username and password when the "Log in" button is pressed.
-            onLoginFailed("Couldn't log in. Configure your App ID and login handler.")
+            val creds = Credentials.emailPassword(username, password)
+            taskApp.loginAsync(creds) {
+                // re-enable the buttons after user login returns a result
+                loginButton.isEnabled = true
+                createUserButton.isEnabled = true
+                if (!it.isSuccess) {
+                    onLoginFailed(it.error.message ?: "An error occurred.")
+                } else {
+                    onLoginSuccess()
+                }
+            }
         }
     }
+
+    private fun loginWithGoogle() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.server_client_id))
+            .requestEmail()
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+        val signInIntent: Intent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_REQUEST) // GOOGLE_LOGIN_REQUEST lets onActivityResult identify the result of THIS call
+    }
+
+
+
+    //------------
+    //AUXILIARY METHODS
+    //------------
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == GOOGLE_LOGIN_REQUEST) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleGoogleSignInResult(task)
+        }
+    }
+
+    private fun handleGoogleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+//            val account: GoogleSignInAccount? = completedTask.result
+            val idToken: String? = account?.idToken
+            val googleCredentials: Credentials = Credentials.google(idToken, GoogleAuthType.ID_TOKEN)
+            taskApp.loginAsync(googleCredentials) {
+                if (it.isSuccess) {
+                    Log.v(
+                        "AUTH",
+                        "Successfully logged in to MongoDB Realm using Google OAuth."
+                    )
+                    onLoginSuccess()
+                } else {
+                    Log.e("AUTH", "Failed to log in to MongoDB Realm", it.error)
+                    onLoginFailed(it.error.message ?: "An error occurred.")
+                }
+            }
+        } catch (e: ApiException) {
+            Log.e("AUTH", "Failed to authenticate using Google OAuth: " + e.message);
+            onLoginFailed(e.message ?: "An error occurred.")
+        }
+    }
+
 }
---
+++ app/src/main/java/com/mongodb/tasktracker/model/login/LoginHelper.kt
@@ -0,0 +1,3 @@
+package com.mongodb.tasktracker.model.login
+
+const val GOOGLE_LOGIN_REQUEST = 1
